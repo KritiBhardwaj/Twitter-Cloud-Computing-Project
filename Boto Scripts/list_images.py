2c40bc8f8cbd536efe9c73c58beb179faa7916bc
CareerDelTorro <sergeygermogentov@rambler.ru>
2018-04-23 20:22:40 +1000

    added boto scripts for launching instances

---
+++ Boto Scripts/list_images.py
@@ -0,0 +1,8 @@
+from nectar_connect import ec2_conn
+
+images = ec2_conn.get_all_images()
+
+for img in images:
+	print("Image ID: {}, image name: {}".format(img.id, img.name))
+
+# Image ID: ami-190a1773, image name: NeCTAR Ubuntu 16.04 LTS (Xenial) amd64
